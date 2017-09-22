c1b0945bb39d424a96ba48ebed19514853e431e6
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-22 14:38:46 -0400

    Updated from Brython Server: 9/22/2017 2:38:46 PM

--- house.py
+++ house.py
@@ -4,4 +4,17 @@
 
 from ggame import *
 
+purple = Color(0x8c03c6,1)
+red = Color(0xFF0000,1)
+yellow = Color(0xfcfc02,1)
+brown = Color(0x3f3f36,1)
+black = Color(0x000000,1)
+
+blackOutline = LineStyle(1,black)
+
+purpleRectangle = RectangleAsset(200,200,blackOutline,purple)
+
+Sprite(purpleRectangle)
+
+
 
