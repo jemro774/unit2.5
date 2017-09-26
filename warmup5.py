ad991d1cd85760c6e86d301c9684432e5d4369c0
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-26 14:19:34 -0400

    Updated from Brython Server: 9/26/2017 2:19:32 PM

--- warmup5.py
+++ warmup5.py
@@ -1,1 +1,11 @@
+#Jack Robey
+#9/26/17
+#warmup5.py - displays a yellow diamond with the user's name inside in blue
 
+from ggame import *
+
+blue = Color(0x0000FF,1)
+
+blackOutline = LineStyle(1,black)
+
+blueDiamond = PolygonAsset([(400,400), (600,600), (800,400), (600,200)],blackOutline,blue)
